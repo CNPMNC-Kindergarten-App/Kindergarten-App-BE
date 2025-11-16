bfa5ea78f963f126abcfb0b2e7c9c871a3000e84
huutuannguyen394 <tuan.nguyenconmevan@hcmut.edu.vn>
2025-11-16 22:44:31 +0700

    Feat: Add API generate student (POST /children)

--- src/main/java/app/mobile/BK_Kindergarten/children/Children.java
+++ src/main/java/app/mobile/BK_Kindergarten/children/Children.java
@@ -4,30 +4,31 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 @Entity
-@Table(name =  "Children")
+@Table(name =  "children")
 @Data
 public class Children {
 
     @Id
     @Column(name = "child_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private  String name;
 
     @Column(name = "sex")
     private  String sex;
 
     @Column(name = "dob")
-    private Date dob;
+    private LocalDate dob;
 
     @Column(name = "habit")
     private String habit;
 
-    @Column(name = "character")
+    @Column(name = "`character`")
     private String character;
 
 //    @OneToOne()
